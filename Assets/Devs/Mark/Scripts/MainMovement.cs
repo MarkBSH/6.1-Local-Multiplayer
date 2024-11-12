eeadbe21049cee4fad9a1510bfc485f61ad78038
MarkBSH <145546641+MarkBSH@users.noreply.github.com>
2024-11-12 16:11:13 +0100

    Fixes + Jump

--- Assets/Devs/Mark/Scripts/MainMovement.cs
+++ Assets/Devs/Mark/Scripts/MainMovement.cs
@@ -6,7 +6,8 @@ using UnityEngine.InputSystem;
 public class MainMovement : MonoBehaviour
 {
     Rigidbody m_RB;
-    public float movementSpeed = 4;
+    public float movementSpeed = 10;
+    public float jumpForce = 10;
     Vector2 moveDir;
 
     void Start()
@@ -16,11 +17,19 @@ public class MainMovement : MonoBehaviour
 
     void Update()
     {
-        m_RB.AddForce(new Vector3(moveDir.x * movementSpeed * Time.deltaTime, 0, moveDir.y * movementSpeed * Time.deltaTime), ForceMode.Force);
+        m_RB.AddForce(new Vector3(moveDir.x * movementSpeed, 0, moveDir.y * movementSpeed), ForceMode.Force);
     }
 
-    public void MoveHorizontal(InputAction.CallbackContext _context)
+    public void GetMovement(InputAction.CallbackContext _context)
     {
         moveDir = _context.ReadValue<Vector2>();
+    }
+
+    public void Jump(InputAction.CallbackContext _context)
+    {
+        if (_context.performed)
+        {
+            m_RB.AddExplosionForce(jumpForce, transform.up, 2f, 2f, ForceMode.Impulse);
+        }
     }
 }
