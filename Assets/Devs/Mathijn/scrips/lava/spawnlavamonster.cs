eeea087c941e6a0d9aa5c41ca14593aa46f0c1eb
Mathijnwis <144341993+Mathijnwis@users.noreply.github.com>
2024-11-15 18:03:14 +0100

    ben verget was ik heb gedaan

--- Assets/Devs/Mathijn/scrips/lava/spawnlavamonster.cs
+++ Assets/Devs/Mathijn/scrips/lava/spawnlavamonster.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class SpawnLavaMonster : MonoBehaviour
 {
@@ -8,6 +9,8 @@ public class SpawnLavaMonster : MonoBehaviour
     public float spawnRadius = 5f;
     public float spawncooldown = 0.5f;
     public bool spawning = false;
+
+    private List<GameObject> spawnedObjects = new List<GameObject>(); 
 
     public void Start()
     {
@@ -29,7 +32,11 @@ public class SpawnLavaMonster : MonoBehaviour
             Vector3 spawnPosition = midelPoint.position + new Vector3(randomDirection.x, 0, randomDirection.y) * spawnRadius;
             Vector3 directionToMiddle = (midelPoint.position - spawnPosition).normalized;
             Quaternion lookRotation = Quaternion.LookRotation(directionToMiddle);
-            Instantiate(prefabToSpawn, spawnPosition, lookRotation);
+            Quaternion finalRotation = lookRotation * Quaternion.Euler(0, 90, 0);
+
+            GameObject spawnedObject = Instantiate(prefabToSpawn, spawnPosition, finalRotation); 
+            spawnedObjects.Add(spawnedObject); 
+
             yield return new WaitForSeconds(spawncooldown);
         }
     }
@@ -37,11 +44,25 @@ public class SpawnLavaMonster : MonoBehaviour
     public void stopspawn()
     {
         spawning = false;
+        DestroyAllSpawnedObjects();
     }
 
     public void startspawn()
     {
         spawning = true;
         StartCoroutine(startspawing());
+
+    }
+
+    public void DestroyAllSpawnedObjects()
+    {
+        foreach (GameObject obj in spawnedObjects)
+        {
+            if (obj != null)
+            {
+                Destroy(obj); 
+            }
+        }
+        spawnedObjects.Clear();
     }
 }
